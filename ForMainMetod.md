67ca9c900976e1c27cb20993d955b5d14febe2bd
vladimir <dd@mail.ru>
2022-12-14 12:56:59 +0300

    обновление

---
+++ ForMainMetod.md
@@ -0,0 +1,2 @@
+
+(ForMainMetod.jpg)
--- zadacha/Program.cs
+++ zadacha/Program.cs
@@ -1,70 +1,24 @@
-﻿
-
-internal class Program
+﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
 {
-    private static void Main(string[] args)
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
     {
-        Console.Write("Сколько элементов Вы хотите ввести? ");
-        int size = Convert.ToInt32(Console.ReadLine());
-        string[] arrayStrings = new string[size];
-        for (int i = size - 1; i >= 0; i--)
-
+    if(array1[i].Length <= 3)
         {
-
-            Console.WriteLine($"Введите {i + 1}-й элемент: ");
-
-            string element = Convert.ToString(Console.ReadLine());
-
-            arrayStrings[i] = element;
-
-        }
-
-
-        string[] arrayFinal = new string[size];
-        int len = 3;
-        int pos = 0;
-
-
-
-        for (int j = 0; j < size; j++)
-        {
-
-            if (arrayStrings[j].Length <= len)
-            {
-
-                arrayFinal[pos] = arrayStrings[j];
-
-                pos++;
-
-            }
-
-        }
-
-
-
-        Console.WriteLine();
-
-        PrintArray(arrayFinal);
-
-
-
-
-
-
-        void PrintArray(string[] array)
-
-        {
-
-            for (int i = 0; i < array.Length; i++)
-
-            {
-
-                Console.Write(array[i] + " ");
-
-            }
-
-            Console.WriteLine();
-
+        array2[count] = array1[i];
+        count++;
         }
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
